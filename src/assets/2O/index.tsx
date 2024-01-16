d3a513c3040376ac9f5093e2a81b39dafc5e3b93
Peixoto <andre.peixoto@trin.ca>
2024-01-16 13:31:55 -0300

    open/closed principle: react example

--- src/assets/2O/index.tsx
+++ src/assets/2O/index.tsx
@@ -7,6 +7,8 @@ type ButtonProps = {
 export function Button({ label }: ButtonProps) {
 	return (
 		<Loading isLoading={false}>
+			// O loading não está dentro do componente button, ele é outro componente que pode ser usado em qualquer parte do
+			// sistema e também permite outras customizações, sem alterar o código do componente button
 			<button>{label}</button>
 		</Loading>
 	)
